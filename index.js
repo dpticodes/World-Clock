3d4e402b34da2f128c1ef90edf9f344a924b22b3
Deepti <deeptiayyar@gmail.com>
2026-02-03 10:57:18 +0100

    added JS to display current time and date info using moment.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  //Mumbai//
+  let mumbaiElement = document.querySelector("#mumbai"); //1. select the main element
+  let mumbaiDateElement = mumbaiElement.querySelector(".date"); //2. select the date and time//
+  let mumbaiTimeElement = mumbaiElement.querySelector(".time");
 
+  let mumbaiTimeZone = moment().tz("Asia/Kolkata"); //define timezone
+
+  mumbaiDateElement.innerHTML = mumbaiTimeZone.format("MMMM Do YYYY"); //3. connect date to the timezone and date format
+  mumbaiTimeElement.innerHTML = `${mumbaiTimeZone.format("h:mm:ss [<small>]A[</small>]")}`; //4.connect time to the time zone and format.
+
+  //berlin//
+  let berlinElement = document.querySelector("#berlin");
+  let berlinDateElement = berlinElement.querySelector(".date");
+  let berlinTimeElement = berlinElement.querySelector(".time");
+
+  let berlinTimeZone = moment().tz("Europe/Berlin");
+
+  berlinDateElement.innerHTML = berlinTimeZone.format("MMMM Do YYYY");
+  berlinTimeElement.innerHTML = `${berlinTimeZone.format("h:mm:ss [<small>]A[</small>]")}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
